c928434aec310b9410cb1f2d1e9cd225e4165b24
William Asaba <asabawilliamdk@yahoo.com>
2025-04-02 13:34:47 +0300

    Add blueprints

---
+++ database/blueprints/TagBlueprint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Blueprints;
+
+use Pocketframe\PocketORM\Data\Blueprint;
+use Faker\Generator;
+
+class TagBlueprint extends Blueprint
+{
+  protected string $entity = \App\Entities\Tag::class;
+
+  public function describe(Generator $faker): array
+  {
+    return [
+      'category_id' => $faker->numberBetween(1, 10),
+      'tag_name' => $faker->unique()->name,
+      'slug' => $faker->unique()->slug(2),
+      'status' => $faker->randomElement(['active', 'inactive']),
+      'description' => $faker->text(100),
+      'created_at' => $faker->dateTimeBetween('-1 year', 'now'),
+      'updated_at' => $faker->dateTimeBetween('-1 year', 'now'),
+    ];
+  }
+}
