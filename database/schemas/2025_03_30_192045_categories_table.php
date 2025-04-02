6fd3ae81a103c43887113d8e474518561e2553ff
William Asaba <asabawilliamdk@yahoo.com>
2025-04-02 13:35:16 +0300

    Add migrations

---
+++ database/schemas/2025_03_30_192045_categories_table.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Schemas;
+
+use Pocketframe\PocketORM\Schema\TableScript;
+use Pocketframe\PocketORM\Schema\TableBuilder;
+
+class Categories extends TableScript
+{
+  public function up(): void
+  {
+    $this->createTable('categories', function (TableBuilder $table) {
+      $table->id();
+      $table->string('category_name')->unique();
+      $table->string('slug')->unique();
+      $table->string('status')->default('active');
+      $table->string('description')->nullable();
+      $table->timestamps();
+    });
+  }
+
+  public function down(): void
+  {
+    $this->dropTable('categories');
+  }
+}
